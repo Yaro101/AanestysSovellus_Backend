6d771dbfc07a6dfd84f4c84a0df2896397a8d878
Yahya Rouissi <yahyarouissi@gmail.com>
2024-11-06 15:02:55 +0200

    adding user schema

---
+++ models/User.js
@@ -0,0 +1,12 @@
+// we can update the schema based on our need
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    votedPolls: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Poll' }]
+});
+
+module.exports = mongoose.model('User', userSchema);
