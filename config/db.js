62af8ca01ef024799aa788976892882340d37804
Yahya Rouissi <yahyarouissi@gmail.com>
2024-11-18 17:51:38 +0200

    Adding db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 // We can make a template so the app can have a db like mongodb instead of local for example or we make a local db.json
 
+// MangoDB url is stored in .env locally so each can test it
+
+// mongoose to interact with MongoDB
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
