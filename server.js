4ae024895df6ccdd78650f4f95db74bbfe8eefd6
Yahya Rouissi <yahyarouissi@gmail.com>
2024-11-19 14:40:06 +0200

    minor adjustment and follow up comments

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+// Use express.json() to parse incoming JSON payloads
 app.use(express.json());
+
+app.use(cors());
 
 connectDB();
 
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/options', require('./routes/pollRoutes'));
+app.use('/api/polls', require('./routes/pollRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // 18.11.24: Added description to dependencies for clarification and maybe useful in the presentation when explaining what we used and why
 // 18.11.24: While changing password try to keep password only alphabetical because special characters need encoding this made quite a mess
+// 19.11.24: moved up express.json() middlware
+// 19.11.24: Testing routes and auths and if the role user/admin is working as it should
+// 19.11.24: Added functionalities to poll schema (created by, timestamp and datestamp and duplicate check for options)
+// 19.11.24: Added validation to prevent multiple votes to pollSchema, added increment and votedBy
